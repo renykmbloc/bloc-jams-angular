8cc567488bef51084698461a9f9b6c7fc240d2fc
Reny Mathew <renykmbloc@gmail.com>
2017-07-15 17:09:49 -0700

    assignment-angular-controllers:complete

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+     function AlbumCtrl() {
+         this.albumData = {};
+         this.albumData = angular.copy(albumPicasso);
+         }
+     
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
