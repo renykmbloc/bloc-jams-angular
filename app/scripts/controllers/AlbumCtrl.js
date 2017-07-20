306c75c6988030eb35950e938b1240f03934d7e8
Reny Mathew <renykmbloc@gmail.com>
2017-07-20 15:30:22 -0700

    checkpoint-angular-services1:complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,11 @@
 (function() {
-     function AlbumCtrl() {
+     function AlbumCtrl(Fixtures) {
          this.albumData = {};
-         this.albumData = angular.copy(albumPicasso);
+         this.albumData = Fixtures.getAlbum();
          }
      
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
  })();
